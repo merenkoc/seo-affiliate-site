c79dccdbc0dc2a7c88cf718b5d8f9d07efce062b
merenkoc <merenkoc55@gmail.com>
2026-04-22 03:11:22 +0300

    fix: add site URL and canonical URL fallback

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://seo-affiliate-site.vercel.app',
   integrations: [
     tailwind(),
   ],
